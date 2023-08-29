c9ce48422b8e7da230a82b5bbebec125563a44ed
Jasper MacNaughton <jaspermacnaughton@gmail.com>
2023-08-29 01:14:05 -0500

    Display full name on landing page

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>My name Jasper</p>
+        <p>My name Jasper MacNaughton.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
